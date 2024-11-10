255af06e01d679d878901ac82c348ba0e9a801bf
Aruj Garg <arujgarg@Arujs-MacBook-Air.local>
2024-11-10 18:31:23 +0530

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
+app.use(express.json()); //parses data in req.body
+app.use(urlencoded({extended: true}));
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
